5dcfac2e64609ef668bfeee265334354110b4bc1
Farmer-OK <lovedrum119@gmail.com>
2023-07-23 12:46:33 +0900

    master 　→ main へ切り替え

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "hsouvarietylifegame": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
